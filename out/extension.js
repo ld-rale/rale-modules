d3b77bb6b94ccbc2f47e2a25cbc0df3e226fdc10
Gobi Dasu <gobi@gobidasu.com>
2022-04-06 18:11:25 -0500

    already highlighting and annotating a couple design patterns

--- out/extension.js
+++ out/extension.js
@@ -8,20 +8,44 @@ const vscode_1 = require("vscode");
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 function activate(context) {
+    let patterns = [/Mixin/g, /Model/g];
+    let patterns_string = ["Mixin", "Model"];
+    let patterns_definitions = [
+        "Mixins let a class adopt methods and attributes of another class.",
+        "MV* (MVC, MVVM, MVP, MVT) divide user interface implemenations into 3 interconnected elements - the model for data related management, the view (or in the case of django the template) for visual representations, and the controller for logic for manipulating the model or view."
+    ];
     let activeEditor = vscode.window.activeTextEditor;
     if (activeEditor) {
         let source_code = activeEditor.document.getText();
-        console.log("activeEditor.document.getText()", source_code);
-        let startLine = 21;
-        let startCharacter = 5;
-        let endLine = 21;
-        let endCharacter = 10;
-        let decorationType = vscode.window.createTextEditorDecorationType({
-            backgroundColor: "yellow"
-        });
-        let rangeOption = new vscode_1.Range(new vscode_1.Position(startLine, startCharacter), new vscode_1.Position(endLine, endCharacter));
-        activeEditor.setDecorations(decorationType, [rangeOption]);
+        //console.log("activeEditor.document.getText()", source_code)
+        for (let i = 0; i < patterns.length; i++) {
+            let match;
+            let pattern = patterns[i];
+            console.log("pattern:", pattern);
+            while (match = pattern.exec(source_code)) {
+                console.log("in while loop");
+                var startPos = activeEditor.document.positionAt(match.index);
+                var endPos = activeEditor.document.positionAt(match.index + match[0].length);
+                let decorationType = vscode.window.createTextEditorDecorationType({
+                    backgroundColor: "yellow"
+                });
+                let rangeOption = new vscode_1.Range(startPos, endPos);
+                activeEditor.setDecorations(decorationType, [rangeOption]);
+            }
+        }
     }
+    vscode.languages.registerHoverProvider('python', {
+        provideHover(document, position, token) {
+            console.log("in provideHover, dpt:", document, position, token);
+            let range = document.getWordRangeAtPosition(position);
+            let word = document.getText(range);
+            console.log("word", word);
+            for (let i = 0; i < patterns_string.length; i++) {
+                if (word.includes(patterns_string[i]))
+                    return new vscode.Hover(patterns_definitions[i]);
+            }
+        }
+    });
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension "ralemodules" is now active2!');
--- src/extension.ts
+++ src/extension.ts
@@ -7,21 +7,49 @@ import { Range, Position } from 'vscode';
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 	
+	let patterns = [/Mixin/g, /Model/g]
+	let patterns_string = ["Mixin", "Model"]
+	let patterns_definitions = [
+		"Mixins let a class adopt methods and attributes of another class.",
+		"MV* (MVC, MVVM, MVP, MVT) divide user interface implemenations into 3 interconnected elements - the model for data related management, the view (or in the case of django the template) for visual representations, and the controller for logic for manipulating the model or view."
+	]
 	let activeEditor = vscode.window.activeTextEditor;
 	if (activeEditor) {
 		let source_code = activeEditor.document.getText()
-		console.log("activeEditor.document.getText()", source_code)
-		let startLine = 21
-		let startCharacter = 5
-		let endLine = 21
-		let endCharacter = 10
-		let decorationType = vscode.window.createTextEditorDecorationType({
-			backgroundColor:"yellow"
-		});
-		let rangeOption = new Range(new Position(startLine, startCharacter), new Position(endLine, endCharacter));
+		//console.log("activeEditor.document.getText()", source_code)
 		
-		activeEditor.setDecorations(decorationType, [rangeOption]);
+		for (let i=0; i<patterns.length; i++){
+			let match;
+			let pattern = patterns[i];
+			console.log("pattern:", pattern);
+			while (match = pattern.exec(source_code)){
+				console.log("in while loop")
+				var startPos = activeEditor.document.positionAt(match.index);
+				var endPos = activeEditor.document.positionAt(match.index + match[0].length);
+
+				let decorationType = vscode.window.createTextEditorDecorationType({
+					backgroundColor:"yellow"
+				});
+				let rangeOption = new Range(startPos, endPos);
+				
+				activeEditor.setDecorations(decorationType, [rangeOption]);
+			}
+		}
 	}
+
+	vscode.languages.registerHoverProvider('python', {
+		provideHover(document, position, token) {
+			console.log("in provideHover, dpt:", document, position, token);
+			let range = document.getWordRangeAtPosition(position);
+			let word = document.getText(range);
+			console.log("word", word);
+			for (let i=0; i<patterns_string.length; i++){
+				if (word.includes(patterns_string[i]))
+					return new vscode.Hover(patterns_definitions[i]);
+			}
+		}
+	});	
+
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "ralemodules" is now active2!');
