158966dd0316fcf8c7648aeae04d60c2fd73e81b
Gobi Dasu <gobi@gobidasu.com>
2022-06-01 18:53:06 -0500

    debugging template model relationship, template highlighting

--- out/parser-py.py
+++ out/parser-py.py
@@ -22,6 +22,8 @@ print("file_to_parse: ", file_to_parse)
 print("folder_to_parse: ", folder_to_parse)
 print("\n")
 
+project_name = folder_to_parse.split("/")[-2]
+
 class PatternArtifact:
     def __init__(self, lineno, col_offset, end_col_offset, file_path): # add the file to all these
         self.lineno = lineno
@@ -48,15 +50,19 @@ class Model(PatternArtifact):
         self.name = name
 
 class View(PatternArtifact):
-    def __init__(self, name, lineno, col_offset, end_col_offset, file_path):
+    def __init__(self, name, lineno, col_offset, end_col_offset, file_path, ast_node=None):
         super().__init__(lineno, col_offset, end_col_offset, file_path)
         self.name = name
+        self.ast_node = ast_node
         self.model = None
 
 class Template(PatternArtifact):
     def __init__(self, name, lineno=-1, col_offset=-1, end_col_offset=-1, file_path=""):
         super().__init__(lineno, col_offset, end_col_offset, file_path)
         self.name = name
+        self.model = None
+        self.view = None
+        self.ast_node = None
 
 # === for mixin pattern ===
 MIXINS = {} # keys are the mixin names, values are Mixin objects
@@ -155,15 +161,15 @@ for t in AST_TREES:
             try:
                 node_name = node.name.lower()
                 if "view" in node_name:
-                    node_name_parts = node_name.split("view")
-                    relevant_view_name = node_name_parts[0]
-                    VIEWS[relevant_view_name] = View(relevant_view_name, node.lineno, -1, -1, t)
+                    # node_name_parts = node_name.split("view")
+                    # relevant_view_name = node_name_parts[0]
+                    VIEWS[node_name] = View(node_name, node.lineno, -1, -1, t, node)
                 else:
                     for b in node.bases:
                         base_name = get_base_name(b)
                         if base_name and type(base_name)==str:
                             if "view" in base_name.lower():
-                                VIEWS[node.name] = View(node.name, node.lineno, -1, -1, t)
+                                VIEWS[node.name] = View(node.name, node.lineno, -1, -1, t, node)
             except:
                 pass
 
@@ -264,10 +270,21 @@ for t in AST_TREES:
 
 # === find the model use in views ===
 for model_name in MODELS:
+    # print("model_name", model_name)
     for view_name in VIEWS:
+        # print("view_name", view_name)
         if model_name in view_name:
             VIEWS[view_name].model = model_name
-                    
+        else: # find out if the model shows up in the View
+            if VIEWS[view_name].ast_node:
+                for node in ast.walk(VIEWS[view_name].ast_node):
+                    try:
+                        if node.name and model_name in node.name:
+                            VIEWS[view_name].model = model_name
+                            break
+                    except: # format not quite right
+                        pass
+
 
 # string search of other file types
 for subdir, dirs, files in os.walk(folder_to_parse):
@@ -276,7 +293,7 @@ for subdir, dirs, files in os.walk(folder_to_parse):
             # === SEARCH FOR TEMPLATES ===
             for temp in TEMPLATES:
                 if temp.lower().strip() == file.lower().strip(): 
-                    print("this file is a template: ", file)
+                    # print("this file is a template: ", file)
                     template_to_add = Template(file, file_path=os.path.join(subdir, file))
                     if file in TEMPLATES:
                         TEMPLATES[file].append(template_to_add)                        
@@ -297,6 +314,12 @@ for subdir, dirs, files in os.walk(folder_to_parse):
                             TEMPLATES[needle] = [template_to_add]
             except:
                 pass
+
+# === find the model use in templates ===
+for model_name in MODELS:
+    for template_name in TEMPLATES:
+        if model_name in template_name:
+            TEMPLATES[template_name].model = model_name
 
 '''
 print("Mixins: ", MIXINS)
@@ -327,7 +350,7 @@ print("TEMPLATES: ", TEMPLATES)
 
 # print all the places we should highlight
 
-jDP = {"mixins": {}, "models": [], "views": [], "templates": [], "classes_by_mixins": {}, "classes_by_propmethods": {}}
+jDP = {"mixins": {}, "models": [], "views": [], "templates": [], "classes_by_mixins": {}, "classes_by_propmethods": {}, "project": project_name}
 CLASSES_BY_MIXINS = {}
 for m in MIXINS:
     mixin = MIXINS[m]
@@ -368,17 +391,18 @@ for m in MODELS:
 for v in VIEWS:
     view = VIEWS[v]
     print(view.name, "view Need2highlight", view.lineno, view.col_offset, view.end_col_offset, view.file_path)
-    jDP["views"].append(view.name)
+    jDP["views"].append({"name": view.name, "model": view.model})
 
 for t in TEMPLATES:
     templates_l = TEMPLATES[t]
     try: # in the case it's a list
         for template in templates_l:
             print(template.name, "template Need2highlight", template.lineno, template.col_offset, template.end_col_offset, template.file_path)
+            jDP["templates"].append({"name": template.name, "model": template.model})
     except: # in the case it's just a single template
         template = templates_l
         print(template.name, "template Need2highlight", template.lineno, template.col_offset, template.end_col_offset, template.file_path)
-    jDP["templates"].append(template.name)
+        jDP["templates"].append({"name": template.name, "model": template.model})
 
 # print("CLASSES_BY_MIXINS", CLASSES_BY_MIXINS)
 jDP["classes_by_propmethods"] = {}
@@ -388,12 +412,103 @@ for code_class in jDP["classes_by_mixins"]:
         for pm_adopted in jDP["classes_by_mixins"][code_class][mixin_adopted]:
             if pm_adopted not in jDP["classes_by_propmethods"][code_class]:
                 jDP["classes_by_propmethods"][code_class].append(pm_adopted)
-print('jDP["classes_by_propmethods"]', jDP["classes_by_propmethods"])
+
+# print('jDP["classes_by_propmethods"]', jDP["classes_by_propmethods"])
 
 response = {"name": folder_to_parse, "details": jDP}
-print("response", response)
+# print("response", response)
 
-with open('out/dp.csv','a') as fd:
+# Constraining Time
+
+# remember jDP = {"mixins": {}, "models": [], "views": [], "templates": [], "classes_by_mixins": {}, "classes_by_propmethods": {}}
+
+# Remove classes that use one or fewer mixins (unless it's the only case of that mixin used alone), or that don't use all the mixins fed into them.
+to_remove = set()
+mixins_used_alone = set()
+for adopter_class in jDP["classes_by_mixins"]:
+    # print(jDP["classes_by_mixins"][adopter_class])
+    if not jDP["classes_by_mixins"][adopter_class]:
+        to_remove.add(adopter_class)
+    if len(jDP["classes_by_mixins"][adopter_class]) < 2: # if using one or fewer mixins
+        for relevant_mixin in jDP["classes_by_mixins"][adopter_class]: # just 1 time
+            if relevant_mixin not in mixins_used_alone:
+                mixins_used_alone.add(relevant_mixin)
+            else:
+                to_remove.add(adopter_class)
+    for adopted_mixin in jDP["classes_by_mixins"][adopter_class]:
+        if not jDP["classes_by_mixins"][adopter_class][adopted_mixin]: # if it's not using all the mixins
+            to_remove.add(adopter_class)
+for adopter_class in to_remove:
+    # print("removing adopter_class:", adopter_class)
+    jDP["classes_by_mixins"].pop(adopter_class)
+
+# Remove mixins that have one or fewer prop methods or adopters. 
+to_remove = set()
+for mixin in jDP["mixins"]:
+    if len(jDP["mixins"][mixin]["prop_methods"]) < 1 or len(jDP["mixins"][mixin]["adopters"]) < 1: # no prop methods or no adopters
+        to_remove.add(mixin)
+for mixin in to_remove:
+    # print("removing mixin:", mixin)
+    jDP["mixins"].pop(mixin)
+
+# Remove mixins that are not reused in our selection.
+to_remove = set()
+use_count = {}
+for adopter_class in jDP["classes_by_mixins"]:
+    for adopted_mixin in jDP["classes_by_mixins"][adopter_class]:
+        if adopted_mixin in use_count:
+            use_count[adopted_mixin] += 1
+        else:
+            use_count[adopted_mixin] = 1
+# print("use_count", use_count)
+
+to_remove = set()
+for adopter_class in jDP["classes_by_mixins"]:
+    remove_class = True
+    for adopted_mixin in jDP["classes_by_mixins"][adopter_class]:
+        if use_count[adopted_mixin] > 1:
+            remove_class = False
+    if remove_class:
+        to_remove.add(adopter_class)
+for adopter_class in to_remove:
+    # print("removing adopter_class:", adopter_class)
+    jDP["classes_by_mixins"].pop(adopter_class)
+
+# Remove activities for mixins / methods that are not in our class selection.
+mixins_used = set()
+prop_method_used = {}
+to_remove = set()
+for adopter_class in jDP["classes_by_mixins"]:
+    for adopted_mixin in jDP["classes_by_mixins"][adopter_class]:
+        mixins_used.add(adopted_mixin)
+        for prop_method in jDP["classes_by_mixins"][adopter_class][adopted_mixin]:
+            if adopted_mixin not in prop_method_used:
+                prop_method_used[adopted_mixin] = [prop_method]
+            else:
+                prop_method_used[adopted_mixin].append(prop_method)
+
+# Remove unused mixins.
+# print("mixins_used:", mixins_used)
+for mixin in jDP["mixins"]:
+    if mixin not in mixins_used:
+        to_remove.add(mixin)
+
+for mixin in to_remove:
+    # print("removing mixin:", mixin)
+    jDP["mixins"].pop(mixin)
+
+# Remove unused mixin prop methods.
+to_remove = set()
+for mixin in jDP["mixins"]:
+    for prop_method in jDP["mixins"][mixin]["prop_methods"]:
+        if prop_method not in prop_method_used[mixin]:
+            to_remove.add((mixin, prop_method))
+# print("to_remove", to_remove)
+for unused_prop_method in to_remove:
+    jDP["mixins"][unused_prop_method[0]]["prop_methods"].remove(unused_prop_method[1])
+
+with open('out/dp.csv','w') as fd:
     writer_object = csv.writer(fd)
+    writer_object.writerow(["File", "Details"])
     writer_object.writerow([folder_to_parse,json.dumps(jDP)])
     fd.close()
